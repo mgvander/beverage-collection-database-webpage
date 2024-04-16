893910af44d1bbd48a34e5ad8ee1029ad013f252
Michael VanderMyde <mvandermyde4231@mail.kvcc.edu>
2024-04-15 23:00:42 -0400

    Connect the program to the Beverage Database.

---
+++ cis237-assignment-6/Models/Beverage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace cis237_assignment_6.Models;
+
+public partial class Beverage
+{
+    [Key]
+    [Column("id")]
+    [StringLength(10)]
+    public string Id { get; set; }
+
+    [Required]
+    [Column("name")]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    [Required]
+    [Column("pack")]
+    [StringLength(20)]
+    public string Pack { get; set; }
+
+    [Column("price", TypeName = "money")]
+    public decimal Price { get; set; }
+
+    [Column("active")]
+    public bool Active { get; set; }
+}
---
+++ cis237-assignment-6/Models/BeverageContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace cis237_assignment_6.Models;
+
+public partial class BeverageContext : DbContext
+{
+    public BeverageContext()
+    {
+    }
+
+    public BeverageContext(DbContextOptions<BeverageContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Beverage> Beverages { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=barnesbrothers.net;Database=BeverageMVandermyde;User Id=mvandermyde;Password=password;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Beverage>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Beverage__3213E83F9B0BC071");
+
+            entity.Property(e => e.Name).IsFixedLength();
+            entity.Property(e => e.Pack).IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
