c97c58e6ad6072a1521a15f1718c4d796a772896
Michael VanderMyde <mvandermyde4231@mail.kvcc.edu>
2024-04-15 23:06:10 -0400

    Create a BeveragesController to the program.

---
+++ cis237-assignment-6/Controllers/BeveragesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using cis237_assignment_6.Models;
+
+namespace cis237_assignment_6.Controllers
+{
+    public class BeveragesController : Controller
+    {
+        private readonly BeverageContext _context;
+
+        public BeveragesController(BeverageContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Beverages
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Beverages.ToListAsync());
+        }
+
+        // GET: Beverages/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Beverages == null)
+            {
+                return NotFound();
+            }
+
+            var beverage = await _context.Beverages
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (beverage == null)
+            {
+                return NotFound();
+            }
+
+            return View(beverage);
+        }
+
+        // GET: Beverages/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Beverages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Pack,Price,Active")] Beverage beverage)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(beverage);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(beverage);
+        }
+
+        // GET: Beverages/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Beverages == null)
+            {
+                return NotFound();
+            }
+
+            var beverage = await _context.Beverages.FindAsync(id);
+            if (beverage == null)
+            {
+                return NotFound();
+            }
+            return View(beverage);
+        }
+
+        // POST: Beverages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,Pack,Price,Active")] Beverage beverage)
+        {
+            if (id != beverage.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(beverage);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BeverageExists(beverage.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(beverage);
+        }
+
+        // GET: Beverages/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Beverages == null)
+            {
+                return NotFound();
+            }
+
+            var beverage = await _context.Beverages
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (beverage == null)
+            {
+                return NotFound();
+            }
+
+            return View(beverage);
+        }
+
+        // POST: Beverages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Beverages == null)
+            {
+                return Problem("Entity set 'BeverageContext.Beverages'  is null.");
+            }
+            var beverage = await _context.Beverages.FindAsync(id);
+            if (beverage != null)
+            {
+                _context.Beverages.Remove(beverage);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BeverageExists(string id)
+        {
+          return _context.Beverages.Any(e => e.Id == id);
+        }
+    }
+}
