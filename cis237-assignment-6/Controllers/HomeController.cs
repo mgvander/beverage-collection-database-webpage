669ac2ce6565f0c755f6a7621a0ff2ac2b738226
Michael VanderMyde <mvandermyde4231@mail.kvcc.edu>
2024-04-17 23:53:43 -0400

    Create a contact page and links to travel to it.
    Correct spelling errors.

--- cis237-assignment-6/Controllers/HomeController.cs
+++ cis237-assignment-6/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace cis237_assignment_6.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
